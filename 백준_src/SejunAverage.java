06d0a237e49acb1055e3b13b45df41cf76777814
Hijiji <wlals1169@naver.com>
2025-09-15 13:58:46 +0900

    백준 - 세준이의 평균속이기

---
+++ 백준_src/SejunAverage.java
@@ -0,0 +1,28 @@
+package 백준_src;
+import java.util.Scanner;
+
+public class SejunAverage {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        //사이즈에 맞는 배열 생성
+        int A[]=new int[N];
+        //max 변수 생성
+        long max=0;
+
+        for(int i=0;i<N;i++){
+            //배열에 next int
+            //더 큰수 찾기 - max에 넣기
+            int t = sc.nextInt();
+            if(max<t)max=t;
+            A[i]= t;
+        }
+        long sum =0;
+        //for 문 한번더 돌면서 합산
+        for(int i=0;i<N;i++){
+            sum += A[i];
+        }
+        //원점수/젤큰수*100/총갯수
+        System.out.println(sum*100.0/max/N);
+    }
+}
