535d71e233f68f17fc2448d7e5fa8292bbe0ecdc
Hijiji <wlals1169@naver.com>
2025-09-30 00:18:13 +0900

    투포인터 좋은수 구하기 구하기 리팩토링

--- do_it/좋은수구하기_1253.java
+++ do_it/좋은수구하기_1253.java
@@ -3,29 +3,36 @@ package do_it;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class 좋은수구하기_1253 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int count = 0;
-        int[] A = new int[N];
+        long[] A = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++){
-            A[i]=Integer.parseInt(br.readLine());
+            A[i]=Long.parseLong(st.nextToken());
         }
+        Arrays.sort(A);
         for(int i = 1; i < N; i++){
             int l=0;
             int m=N-1;
             while(l<m) {
                 if (A[i]==A[l]+A[m]){
-                    count++;
+                        count++;
+                        break;
                 }else if(A[i]>A[l]+A[m]){
-                    m--;
-                }else{
                     l++;
+
+                }else{
+                    m--;
                 }
             }
         }
         System.out.println(count);
+        br.close();
     }
 }
