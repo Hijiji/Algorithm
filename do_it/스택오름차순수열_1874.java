1342ee6b8d12c5635f6c67eecbeb01c30988ee9c
Hijiji <wlals1169@naver.com>
2025-10-14 18:55:42 +0900

    stack - 오름차순 수열 문제풀이

---
+++ do_it/스택오름차순수열_1874.java
@@ -0,0 +1,40 @@
+package do_it;
+import java.io.*;
+import java.util.*;
+
+public class 스택오름차순수열_1874 {
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        int N  = scanner.nextInt();
+        int[] a = new int[N];
+        for(int i=0; i<N; i++){
+            a[i] = scanner.nextInt();
+        }
+        int[] c = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
+        int cN = 1;
+        for(int i=0; i<N; i++){
+            if(a[i]>=cN) {
+                while (a[i] >= cN) {
+                    stack.push(cN++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            }else{
+                int n = stack.pop();
+                if(n>cN) {
+                    System.out.print("NO");
+                    break;
+                }else{
+                    bf.append("-\n");
+                }
+            }
+        }
+        System.out.print(bf.toString());
+    }
+
+    //a 랑 c 랑 같은지 비교 : 같으면 넘어감, 다르면 No출력
+
+}
